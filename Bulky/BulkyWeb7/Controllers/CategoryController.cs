6f2381977206e8229da7bd7d1e3b326aacf04034
Wiktor Szymulewicz <wiktor@szymulewicz.com>
2023-04-01 00:51:17 +0200

    Category CRUD

---
+++ Bulky/BulkyWeb7/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+﻿using BulkyWeb7.Data;
+using BulkyWeb7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb7.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category objCategory)
+        {
+            if (objCategory.Name == objCategory.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(objCategory);
+                _db.SaveChanges();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(objCategory);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category objCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(objCategory);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(objCategory);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
